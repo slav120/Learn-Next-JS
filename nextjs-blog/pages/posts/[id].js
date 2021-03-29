a1fabac56739057ea3aef9b5e5bd4ecd8b19641c
Slav Lalev <slav@4thwhale.com>
2021-03-29 12:10:21 -0400

    added more styling to our id routes / pages

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -2,6 +2,8 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
 
 
 
@@ -26,15 +28,16 @@ export async function getStaticPaths() {
   export default function Post({ postData }) {
     return (
       <Layout>
-          <Head>
-        {postData.title}
-          </Head>
-        <br />
-        {postData.id}
-        <br />
-        <Date dateString={postData.date} />
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
       </Layout>
     )
   }
