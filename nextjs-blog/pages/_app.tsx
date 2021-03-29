fb12f89889aa0ede12e442cfc84316befd2a5f91
Slav Lalev <slav@4thwhale.com>
2021-03-29 13:57:19 -0400

    converted project to typescript

---
+++ nextjs-blog/pages/_app.tsx
@@ -0,0 +1,22 @@
+// // import '../styles/global.css'
+
+
+// // export default function App({ Component, pageProps }) {
+// //     return <Component {...pageProps} />
+// //   }
+
+// import { AppProps } from 'next/app'
+
+// function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+// export default App
+
+
+import '../styles/global.css'
+import { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
